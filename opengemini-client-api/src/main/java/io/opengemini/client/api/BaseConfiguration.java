a5eeadb7e3703a0efc98dad59c8993ffe8b88abf
zhiheng123 <903292776@qq.com>
2023-11-30 22:20:42 +0800

    style: add builder for the configure of AuthConfig and BaseConfiguration (#8)
    Signed-off-by: zhiheng123 <903292776@qq.com>

--- opengemini-client-api/src/main/java/io/opengemini/client/api/BaseConfiguration.java
+++ opengemini-client-api/src/main/java/io/opengemini/client/api/BaseConfiguration.java
@@ -1,13 +1,20 @@
 package io.opengemini.client.api;
 
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 
 import java.util.List;
 
 @Setter
 @Getter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BaseConfiguration {
     List<Address> addresses;
 
--- opengemini-client-reactor/src/main/java/io/opengemini/client/reactor/Configuration.java
+++ opengemini-client-reactor/src/main/java/io/opengemini/client/reactor/Configuration.java
@@ -1,6 +1,10 @@
 package io.opengemini.client.reactor;
 
 import io.opengemini.client.api.BaseConfiguration;
+import lombok.AllArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
+@SuperBuilder
+@AllArgsConstructor
 public class Configuration extends BaseConfiguration {
 }
