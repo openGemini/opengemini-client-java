1d8a3a1a3c534527f2659c7634d8e32fdef48889
moxiaoying <1159230165@qq.com>
2024-06-18 21:32:40 +0800

    refactor: reusing keystore and truststore in test common
    Signed-off-by: moxiaoying <1159230165@qq.com>

---
+++ opengemini-client-test-common/src/main/java/utils/TlsTestUtil.java
@@ -0,0 +1,19 @@
+package utils;
+
+public class TlsTestUtil {
+    public static final String JKS_PASSWORD = "TestOpenGemini@#123";
+    private static final String ABSOLUTE_RESOURCE_PATH =
+        TlsTestUtil.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+
+    public static String getResourcePathOfKeyStoreJks() {
+        return ABSOLUTE_RESOURCE_PATH + "/jks/keystore.jks";
+    }
+
+    public static String getResourcePathOfTrustStoreJks() {
+        return ABSOLUTE_RESOURCE_PATH + "/jks/truststore.jks";
+    }
+
+    public static String getJksPassword() {
+        return JKS_PASSWORD;
+    }
+}
