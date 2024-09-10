9dada57bbe34fc3abc4a35f83cfb44651a118094
cpcchengt <chengt@cpchain.io>
2024-09-10 16:41:00 +0800

    fix: add health check
    Signed-off-by: cpcchengt <chengt@cpchain.io>

--- opengemini-client-common/src/main/java/io/opengemini/client/common/BaseClient.java
+++ opengemini-client-common/src/main/java/io/opengemini/client/common/BaseClient.java
@@ -5,10 +5,19 @@ import io.opengemini.client.api.BaseConfiguration;
 import io.opengemini.client.api.Endpoint;
 import io.opengemini.client.api.Query;
 
+import java.net.URI;
 import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -16,6 +25,10 @@ public abstract class BaseClient {
     private final List<Endpoint> serverUrls = new ArrayList<>();
 
     private final AtomicInteger prevIndex = new AtomicInteger(-1);
+
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(2);
+
+    private final HttpClient healthCheckClient = HttpClient.newHttpClient();
 
     public BaseClient(BaseConfiguration conf) {
         String httpPrefix;
@@ -28,10 +41,52 @@ public abstract class BaseClient {
             String url = httpPrefix + address.getHost() + ":" + address.getPort();
             this.serverUrls.add(new Endpoint(url, new AtomicBoolean(false)));
         }
+        startHealthCheck();
+    }
+
+    /**
+     * Health Check
+     * Start schedule task(period 10s) to ping all server url
+     */
+    private void startHealthCheck() {
+        scheduler.scheduleWithFixedDelay(new Runnable() {
+            @Override
+            public void run() {
+                for (Endpoint url : serverUrls) {
+                    try {
+                        HttpRequest request = HttpRequest.newBuilder()
+                                .uri(URI.create(url.getUrl() + getPingUrl()))
+                                .GET()
+                                .timeout(Duration.ofSeconds(2))
+                                .build();
+                        HttpResponse<String> response = healthCheckClient
+                                .sendAsync(request, HttpResponse.BodyHandlers.ofString()).get();
+                        if (response.statusCode() >= 200 && response.statusCode() < 300) {
+                            url.setIsDown(new AtomicBoolean(false));
+                        } else {
+                            url.setIsDown(new AtomicBoolean(true));
+                        }
+                    } catch (ExecutionException | InterruptedException e) {
+                        url.setIsDown(new AtomicBoolean(true));
+                    }
+                }
+            }
+        }, 0, 10, TimeUnit.SECONDS);
+    }
+
+    protected void stopHealthCheck() {
+        scheduler.shutdown();
     }
 
     protected String nextUrlPrefix() {
         int idx = Math.abs(prevIndex.incrementAndGet() % serverUrls.size());
+        for (int i = 0; i < serverUrls.size(); i++) {
+            if (!serverUrls.get(idx).getIsDown().get()) {
+                return serverUrls.get(idx).getUrl();
+            } else {
+                idx = Math.abs(prevIndex.incrementAndGet() % serverUrls.size());
+            }
+        }
         return serverUrls.get(idx).getUrl();
     }
 
