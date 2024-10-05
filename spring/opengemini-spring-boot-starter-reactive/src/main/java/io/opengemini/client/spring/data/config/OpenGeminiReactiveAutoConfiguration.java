e19a2becc298e29f2b04b44a236cc503866fdda8
weiping-code <hustwaitone@163.com>
2024-10-05 12:12:48 +0800

    chore: set conditional on missing bean by class (#134)
    Signed-off-by: weiping-code <hustwaitone@163.com>

--- spring/opengemini-spring-boot-starter-reactive/src/main/java/io/opengemini/client/spring/data/config/OpenGeminiReactiveAutoConfiguration.java
+++ spring/opengemini-spring-boot-starter-reactive/src/main/java/io/opengemini/client/spring/data/config/OpenGeminiReactiveAutoConfiguration.java
@@ -1,26 +1,29 @@
 package io.opengemini.client.spring.data.config;
 
 import io.opengemini.client.spring.data.core.DefaultOpenGeminiSerializerFactory;
+import io.opengemini.client.spring.data.core.OpenGeminiProperties;
 import io.opengemini.client.spring.data.core.OpenGeminiSerializerFactory;
 import io.opengemini.client.spring.data.core.ReactiveOpenGeminiTemplate;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import reactor.core.publisher.Flux;
 
 @AutoConfiguration
 @ConditionalOnClass({ReactiveOpenGeminiTemplate.class, Flux.class})
+@EnableConfigurationProperties(OpenGeminiProperties.class)
 public class OpenGeminiReactiveAutoConfiguration {
 
     @Bean
-    @ConditionalOnMissingBean(name = "reactiveOpenGeminiTemplate")
+    @ConditionalOnMissingBean(ReactiveOpenGeminiTemplate.class)
     public ReactiveOpenGeminiTemplate reactiveOpenGeminiTemplate() {
         return new ReactiveOpenGeminiTemplate();
     }
 
     @Bean
-    @ConditionalOnMissingBean(name = "openGeminiSerializerFactory")
+    @ConditionalOnMissingBean(OpenGeminiSerializerFactory.class)
     public OpenGeminiSerializerFactory openGeminiSerializerFactory() {
         return new DefaultOpenGeminiSerializerFactory();
     }
--- spring/opengemini-spring/src/main/java/io/opengemini/client/spring/data/core/MeasurementOperations.java
+++ spring/opengemini-spring/src/main/java/io/opengemini/client/spring/data/core/MeasurementOperations.java
@@ -1,15 +1,14 @@
 package io.opengemini.client.spring.data.core;
 
-import io.opengemini.client.api.OpenGeminiException;
 import io.opengemini.client.api.Query;
 
 import java.util.List;
 
 public interface MeasurementOperations<T> {
 
-    void write(T t) throws OpenGeminiException;
+    void write(T t);
 
-    void write(List<T> list) throws OpenGeminiException;
+    void write(List<T> list);
 
     List<T> query(Query query);
 }
