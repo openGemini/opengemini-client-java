963260e83ca2bdf6a073f4e9cdbb02db827c5090
castlighting <castlight1@protonmail.com>
2024-10-30 23:41:22 +0800

    feat: RpcClient

--- opengemini-client-grpc/src/main/java/io/opengemini/client/grpc/RpcClient.java
+++ opengemini-client-grpc/src/main/java/io/opengemini/client/grpc/RpcClient.java
@@ -1,4 +1,18 @@
 package io.opengemini.client.grpc;
 
+import io.opengemini.client.api.RpcClientConfig;
+import io.opengemini.client.grpc.support.RpcClientSupplier;
+
 public class RpcClient {
+    private final RpcClientConfig config;
+
+
+
+    public static RpcClient create(final RpcClientConfig config) {
+        return new RpcClient(config);
+    }
+
+    private RpcClient(RpcClientConfig config) {
+        this.config = config;
+    }
 }
