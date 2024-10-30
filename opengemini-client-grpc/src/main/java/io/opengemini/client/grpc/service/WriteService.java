975a1a328a348c4e8061e3eb8d1e16a6c8a2482a
castlighting <castlight1@protonmail.com>
2024-10-30 23:41:09 +0800

    feat: WriteService Impl

---
+++ opengemini-client-grpc/src/main/java/io/opengemini/client/grpc/service/WriteService.java
@@ -0,0 +1,30 @@
+package io.opengemini.client.grpc.service;
+
+import io.opengemini.client.api.OpenGeminiException;
+import io.opengemini.client.grpc.ResponseCode;
+import io.opengemini.client.grpc.RpcClientConnectionManager;
+import io.opengemini.client.grpc.VertxWriteServiceGrpc;
+import io.opengemini.client.grpc.WriteRowsRequest;
+
+import java.util.concurrent.CompletableFuture;
+
+public class WriteService extends ServiceImpl {
+    private final VertxWriteServiceGrpc.WriteServiceVertxStub stub;
+
+    protected WriteService(RpcClientConnectionManager connectionManager) {
+        super(connectionManager);
+        this.stub = connectionManager.newStub(VertxWriteServiceGrpc::newVertxStub);
+    }
+
+
+    public CompletableFuture<Void> writeRows(WriteRowsRequest writeRowsRequest) {
+        return execute(() -> stub.writeRows(writeRowsRequest), writeRowsResponse -> {
+            if (ResponseCode.Success == writeRowsResponse.getCode()) {
+                return null;
+            } else {
+                // TODO: Exception ?
+                return null;
+            }
+        });
+    }
+}
