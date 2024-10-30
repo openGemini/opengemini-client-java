943c2bc919b2622bbdf8176b48e28b3961e53486
castlighting <castlight1@protonmail.com>
2024-10-30 23:40:06 +0800

    feat: RpcClientSupplier

---
+++ opengemini-client-grpc/src/main/java/io/opengemini/client/grpc/support/RpcClientSupplier.java
@@ -0,0 +1,43 @@
+package io.opengemini.client.grpc.support;
+
+import java.util.Objects;
+import java.util.function.Supplier;
+
+public class RpcClientSupplier<T> implements Supplier<T> {
+    private final Supplier<T> delegate;
+    private volatile boolean initialized = false;
+    private T holder;
+
+    public RpcClientSupplier(Supplier<T> delegate) {
+        this.delegate = Objects.requireNonNull(delegate);
+    }
+
+    @Override
+    public T get() {
+        if (!initialized) {
+            synchronized (this) {
+                if (!initialized) {
+                    T t = delegate.get();
+                    holder = t;
+                    initialized = true;
+                    return t;
+                }
+            }
+        }
+        return holder;
+    }
+
+    public void close() {
+        if (initialized) {
+            synchronized (this) {
+                if (initialized) {
+                    if (holder != null) {
+                        holder = null;
+                        // close
+                    }
+                }
+                initialized = false;
+            }
+        }
+    }
+}
