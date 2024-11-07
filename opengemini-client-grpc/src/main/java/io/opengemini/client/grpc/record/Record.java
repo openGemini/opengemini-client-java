4bb8df38d5e8fb9c628055f9802ab3f14766c298
castlighting <castlight1@protonmail.com>
2024-11-07 22:19:02 +0800

    doc: record binary structure

--- opengemini-client-grpc/src/main/java/io/opengemini/client/grpc/record/Record.java
+++ opengemini-client-grpc/src/main/java/io/opengemini/client/grpc/record/Record.java
@@ -6,6 +6,40 @@ import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
+/**
+ * Record Binary Structure:
+ * +----------------------------------------------------------------------------------------+
+ * |                                   Record Header                                        |
+ * +----------------------------------------------------------------------------------------+
+ * |  Schema Length  |    Schema Array     |   ColVals Length   |      ColVals Array        |
+ * |    (4 bytes)   |   (variable size)   |     (4 bytes)      |    (variable size)         |
+ * +----------------------------------------------------------------------------------------+
+ *
+ * Schema Array Structure (repeated for each field):
+ * +----------------------------------------------------------------------------------------+
+ * |  Field Size  |    Type    |   Name Length  |              Name                         |
+ * |   (4 bytes)  | (4 bytes)  |   (4 bytes)   |     (variable bytes, UTF-8)                |
+ * +----------------------------------------------------------------------------------------+
+ *
+ * ColVals Array Structure (repeated for each column):
+ * +----------------------------------------------------------------------------------------+
+ * |  ColVal Size |                          ColVal Content                                 |
+ * |   (4 bytes)  |                                                                         |
+ * |              |  +----------------------------------------------------------+           |
+ * |              |  | Val Length  |  Val Content   |                           |           |
+ * |              |  | (4 bytes)   | (variable)     |                           |           |
+ * |              |  +----------------------------------------------------------+           |
+ * |              |  | Offset Length  |  Offset Array                           |           |
+ * |              |  | (4 bytes)      | (4 bytes * length)                      |           |
+ * |              |  +----------------------------------------------------------+           |
+ * |              |  | Bitmap Length  |  Bitmap                                 |           |
+ * |              |  | (4 bytes)      | (variable bytes)                        |           |
+ * |              |  +----------------------------------------------------------+           |
+ * |              |  | BitMapOffset   |  Length        |  NilCount              |           |
+ * |              |  | (4 bytes)      |  (4 bytes)     |  (4 bytes)            |            |
+ * |              |  +----------------------------------------------------------+           |
+ * +----------------------------------------------------------------------------------------+
+ */
 @Data
 public class Record {
     private RecMeta recMeta;
