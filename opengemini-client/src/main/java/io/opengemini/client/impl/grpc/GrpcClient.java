3dc2806a52001d0b54be5adaeacb92ae9ab7122f
castlighting <castlight1@protonmail.com>
2024-12-17 15:16:13 +0800

    feat: Integrate GrpcClientConnectionManager into GrpcClient to manage connection lifecycle and resource cleanup.

--- opengemini-client/src/main/java/io/opengemini/client/impl/grpc/GrpcClient.java
+++ opengemini-client/src/main/java/io/opengemini/client/impl/grpc/GrpcClient.java
@@ -4,6 +4,7 @@ import io.opengemini.client.api.GrpcConfig;
 
 public class GrpcClient {
     private final GrpcConfig config;
+    private final GrpcClientConnectionManager connectionManager;
 
     public static GrpcClient create(final GrpcConfig config) {
         return new GrpcClient(config);
@@ -11,6 +12,7 @@ public class GrpcClient {
 
     private GrpcClient(GrpcConfig config) {
         this.config = config;
+        this.connectionManager = new GrpcClientConnectionManager(config);
     }
 
 }
